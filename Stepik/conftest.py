7bae38732e6dc213b32b9a8ab5ecb17427a19391
QA-Vlad <kuznetcov.vladlen@gmail.com>
2024-05-17 13:05:57 +0700

    Set up the environment

---
+++ Stepik/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")  # Выбор браузера в командной строке: pytest --browser_name=firefox
+    parser.addoption('--language', action='store', default='en',  #Выбор языка в командной строке --language=ru
+                     help='Choose language: es or fr or en or ru')
+
+
+@pytest.fixture(scope="function")
+# #Что-бы работало быстрее, но кушало оперативу заменить, закомментировать строку выше и раскомментировать ниже
+# @pytest.fixture(scope="class")
+def browser(request):
+    print("conftest.py is loaded")
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        print("\nТест кейс в Хроме начался...")
+        options = webdriver.ChromeOptions()
+        options.add_argument(f'--lang={user_language}')
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nТест кейс в Фаерфох начался...")
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=firefox_options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+    print("\nТест кейс окончился")
+    browser.quit()
