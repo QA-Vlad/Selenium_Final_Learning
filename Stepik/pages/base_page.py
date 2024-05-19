50cf26e3987f67f78747034039049761cf0afeac
QA-Vlad <kuznetcov.vladlen@gmail.com>
2024-05-19 18:30:46 +0700

    Add new tests

--- Stepik/pages/base_page.py
+++ Stepik/pages/base_page.py
@@ -3,6 +3,7 @@ import pyperclip
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -61,3 +62,10 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
+    # Метод перехода на страницу логина
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылка логина не найдена"
--- Stepik/test_product_page.py
+++ Stepik/test_product_page.py
@@ -21,26 +21,40 @@ link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 # Проверял что после добавление товара в корзину не появляется сообщение об этом (отрицательное тестирование)
-@pytest.mark.xfail(reason="Ожидаемо падающий негативный тест")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    product_page = PageObject(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
-    product_page.should_not_be_success_message()
+# @pytest.mark.xfail(reason="Ожидаемо падающий негативный тест")
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     product_page = PageObject(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.should_not_be_success_message()
 
 
 # Проверяет что при открытии странице нет сообщения об успешное добавление товара в корзину
-def test_guest_cant_see_success_message(browser):
-    product_page = PageObject(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
+# def test_guest_cant_see_success_message(browser):
+#     product_page = PageObject(browser, link)
+#     product_page.open()
+#     product_page.should_not_be_success_message()
 
 
 # Проверяет что после появление сообщение о добавление товара в корзину, это сообщение пропадает через несколько сек
 # (негативное тестирование)
-@pytest.mark.xfail(reason="Ожидаемо падающий негативный тест")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    product_page = PageObject(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
-    product_page.should_disappear_element()
+# @pytest.mark.xfail(reason="Ожидаемо падающий негативный тест")
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     product_page = PageObject(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.should_disappear_element()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.go_to_login_page()
